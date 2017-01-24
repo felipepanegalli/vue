cd976967771d10fa945645598711fa33daf0ea83
Felipe Panegalli <felipe.panegalli@gmail.com>
2017-01-24 11:17:04 -0200

    - Adicionado a alteração de status - Adicionado o :class das cores - Adicionado modal (Bug no excluir)

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
-var app = new Vue({
+app = new Vue({
 	el: "#app",
 	data: {
+		showModal: false,
 		test: "",
 		title: "Contas a receber",
 		menus: [
-			{id: 0, name: "Listar contas"}, 
+			{id: 0, name: "Listar contas"},
 			{id: 1, name: "Criar conta"}
 		],
 		activedView: 1,
 		formType: 'insert',
-		bill:{
+		bill: {
 			date_due: '',
 			name: '',
 			value: 0,
 			done: 0
 		},
-		names:[
+		names: [
 			'Conta de Luz',
 			'Conta de água',
 			'Conta de telefone',
@@ -24,54 +25,61 @@ var app = new Vue({
 			'Empréstimo',
 			'Gasolina'
 		],
+		payment: [
+			{id: 0, name: "Não Paga"},
+			{id: 1, name: "Paga"}
+		],
 		bills: [
-			{date_due: '20/08/2016', name: 'Conta de Luz', value: 70, done:0},
-			{date_due: '21/08/2016', name: 'Conta de água', value: 50, done:1},
-			{date_due: '22/08/2016', name: 'Conta de telefone', value: 55.99, done:0},
-			{date_due: '23/08/2016', name: 'Supermercado', value: 603.99, done:1},
-			{date_due: '24/08/2016', name: 'Cartão de crádito', value: 1225.99, done:0},
-			{date_due: '25/08/2016', name: 'Empréstimo', value: 2000, done:0},
-			{date_due: '26/08/2016', name: 'Gasolina', value: 200, done:1}
+			{date_due: '20/08/2016', name: 'Conta de Luz', value: 70, done: 0},
+			{date_due: '21/08/2016', name: 'Conta de água', value: 50, done: 1},
+			{date_due: '22/08/2016', name: 'Conta de telefone', value: 55.99, done: 0},
+			{date_due: '23/08/2016', name: 'Supermercado', value: 603.99, done: 1},
+			{date_due: '24/08/2016', name: 'Cartão de crádito', value: 1225.99, done: 0},
+			{date_due: '25/08/2016', name: 'Empréstimo', value: 2000, done: 0},
+			{date_due: '26/08/2016', name: 'Gasolina', value: 200, done: 1}
 		]
 	},
-	computed:{
-		status: function(){
+	computed: {
+		status: function () {
 			var count = 0;
-			for(var i in this.bills){
-				if(!this.bills[i].done){
+			for (var i in this.bills) {
+				if (!this.bills[i].done) {
 					count++;
 				}
 			}
-			return !count?"Nenhuma conta a pagar" : "Existem " + count + " a serem pagas";
+			if (this.bills == 0) {
+				return "Nenhuma conta cadastrada"
+			}
+			return !count ? "Nenhuma conta a pagar" : "Existem " + count + " a serem pagas";
 		}
 	},
-	methods:{
-		showView: function (id){
+	methods: {
+		showView: function (id) {
 			this.activedView = id;
-			if(id == 1){
+			if (id == 1) {
 				this.formType = 'insert';
 			}
 		},
-		submit: function(){
-			if(this.formType == 'insert'){
+		submit: function () {
+			if (this.formType == 'insert') {
 				this.bills.push(this.bill);
 			}
 
 			this.bill = {
-			date_due: '',
-			name: '',
-			value: 0,
-			done: 0
+				date_due: '',
+				name: '',
+				value: 0,
+				done: 0
 			};
 
 			this.activedView = 0;
 		},
-		loadBill: function (id){
+		loadBill: function (id) {
 			this.bill = id;
 			this.activedView = 1;
 			this.formType = 'update';
 		},
-		deleteBill: function (id){
+		deleteBill: function (id) {
 			this.bills.$remove(id);
 		}
 	}
@@ -84,3 +92,16 @@ Vue.filter('doneLabel', function(value){
 		return "Paga"
 	}
 });
+
+Vue.component('modal', {
+  template: '#modal-template',
+  props: {
+    show: {
+      type: Boolean,
+      required: true,
+      twoWay: true    
+    }
+  }
+});
+
+//<a href="#" @click.prevent="deleteBill(o)">Excluir</a>
